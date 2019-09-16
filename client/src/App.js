3f40dd177a95e64392c7ec22341c59805ebe49cb
Anthony Hill <hillan1152@gmail.com>
2019-09-16 16:12:47 -0400

    Inital set up of movie list and movie id in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Route } from "react-router-dom";
 
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -13,6 +16,9 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+
+      <Route exact path="/" component={MovieList}/>
+      <Route path="/movies/:id" component={Movie}/>
     </div>
   );
 };
